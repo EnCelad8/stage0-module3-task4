7b0a11dd6ef3d6a0d0c409588a2cc44663c9d45d
EnCelad8 <naxmax70@gmail.com>
2023-11-16 00:22:02 +0300

    task 4

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int cValue = 99;
+        int hValue = 104;
+        int aValue = 97;
+        int rValue = 114;
+
+
+        System.out.println((char) cValue);
+        System.out.println((char) hValue);
+        System.out.println((char) aValue);
+        System.out.println((char) rValue);
     }
 }
